7a0dbb797d60e6e130b80c3dbea90989d349de67
Pearoo <63553775+rnxm@users.noreply.github.com>
2025-09-22 14:56:12 +0200

    added docstring to core library

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,13 @@
-from core.argument import Argument
-from core.command import Command
-from core.slash_localization import SlashCommandLocalizer, update_slash_localizations, slash_command_localization
-from core.context import Context
-from core.bot import MyClient
+"""
+Core functionality needed for the bot to run.
+
+This library exists to avoid circular imports and to clean up the mess that could be seen previously
+in main.py. It's mostly used for type hints (especially MyClient and Context).
+"""
+# ruff: noqa F403
+
+from core.argument import *
+from core.command import *
+from core.slash_localization import *
+from core.context import *
+from core.bot import *
